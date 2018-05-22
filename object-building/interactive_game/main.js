74894f7bc73290f330ebe9766926653f8662b8d2
Sílvia Baptista <silvia.baptista@mainbrain.pt>
2018-05-22 16:08:14 +0100

    Necessary changes

--- object-building/interactive_game/main.js
+++ object-building/interactive_game/main.js
@@ -17,17 +17,17 @@ function Shape(x,y,velX,velY,exists){
 }
 
 function Ball(x,y,velX,velY,exists,size,color){
-    Shape.call(this, x,y,velX,velY,exists);
+    Shape.call(this,x,y,velX,velY,exists);
     this.size = size;
     this.color = color;
 }
 
 Ball.prototype = Object.create(Shape.prototype);
 Ball.prototype.constructor = Ball;
+
 Ball.prototype.draw = function(){
     ctx.beginPath();
     ctx.fillStyle = this.color;
-
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
 }
@@ -61,37 +61,111 @@ Ball.prototype.collisionDetect = function() {
             var distance = Math.sqrt(dx * dx + dy * dy);
 
             if (distance < this.size + balls[j].size) {
-                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+                //balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
             }
         }
     }
 }
 
+/*function EvilCircle(x,y,exists){
+    Shape.call(this,x,y,20,20,exists);
+    this.size = 10;
+    this.color = 'white';
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+EvilCircle.prototype.draw = function(){
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+EvilCircle.prototype.checkBounds = function() {
+    if ((this.x + this.size) >= width) {
+        this.x = -(this.size);
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.x = -(this.size);
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.y = -(this.size);
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.y = -(this.size);
+    }
+}
+
+EvilCircle.prototype.setControls = function(){
+    var _this = this;
+    window.onkeydown = function(e) {
+        if (e.keyCode === 65) {
+            _this.x -= _this.velX;
+        } else if (e.keyCode === 68) {
+            _this.x += _this.velX;
+        } else if (e.keyCode === 87) {
+            _this.y -= _this.velY;
+        } else if (e.keyCode === 83) {
+            _this.y += _this.velY;
+        }
+    }
+}
+
+EvilCircle.prototype.collisionDetect = function() {
+    for (var j = 0; j < balls.length; j++) {
+        if (balls[j].exists) {
+            var dx = this.x - balls[j].x;
+            var dy = this.y - balls[j].y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].exists = false;
+            }
+        }
+    }
+}*/
+
 var balls = [];
 
+/*var evil = new EvilCircle(random(0,width), random(0,height), true);
+evil.setControls();*/
+
 function loop() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = 'rgba(0,0,0,0.25)';
+    ctx.fillRect(0,0,width,height);
 
-    while (balls.length < 25) {
+    while(balls.length < 25) {
         var size = random(10,20);
-
         var ball = new Ball(
             random(0 + size,width - size),
             random(0 + size,height - size),
             random(-7,7),
             random(-7,7),
+            true,
             'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
             size
         );
         balls.push(ball);
     }
 
-    for (var i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
-        balls[i].collisionDetect();
+    for(var i = 0; i < balls.length; i++) {
+        //if(balls[i].exists) {
+            balls[i].draw();
+            balls[i].update();
+            balls[i].collisionDetect();
+        //}
     }
+
+    /*evil.draw();
+    evil.checkBounds();
+    evil.collisionDetect();*/
 
     requestAnimationFrame(loop);
 }
